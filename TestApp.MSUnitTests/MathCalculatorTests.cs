ddefb737db6ee27a768755f83ef5170f2a496216
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-10-04 11:51:43 +0200

    Utworzenie testów kalkulatorów

---
+++ TestApp.MSUnitTests/MathCalculatorTests.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp.MSUnitTests
+{
+    [TestClass]
+    public class MathCalculatorTests
+    {
+        // Method_Scenario_ExpectedBehavior
+
+        [TestMethod]
+        public void Add_PassedArguments_ReturnsSumOfAguments()
+        {
+            // Arrange
+            MathCalculator mathCalculator = new MathCalculator();
+
+            // Act
+            var result = mathCalculator.Add(1, 2);
+
+            // Assert
+            Assert.AreEqual( 3, result);
+        }
+
+        [TestMethod]
+        public void Max_FirstArgumentIsGreater_ReturnsFirstArgument()
+        {
+            // Arrange
+            MathCalculator mathCalculator = new MathCalculator();
+
+            // Act
+            var result = mathCalculator.Max(2, 1);
+
+            // Assert
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void Max_SecondArgumentIsGreater_ReturnsSecondArgument()
+        {
+            // Arrange
+            MathCalculator mathCalculator = new MathCalculator();
+
+            // Act
+            var result = mathCalculator.Max(1, 2);
+
+            // Assert
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void Max_ArgumentsAreEqual_ReturnsFirstArgument()
+        {
+            // Arrange
+            MathCalculator mathCalculator = new MathCalculator();
+
+            // Act
+            var result = mathCalculator.Max(1, 1);
+
+            // Assert
+            Assert.AreEqual(1, result);
+        }
+    }
+}
