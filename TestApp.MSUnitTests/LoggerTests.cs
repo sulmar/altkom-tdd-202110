dba587fd23fbc6b445bbddc77bc6f5544ec8b63e
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-10-04 12:23:50 +0200

    Testowanie metody void

---
+++ TestApp.MSUnitTests/LoggerTests.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp.MSUnitTests
+{
+    [TestClass]
+    public class LoggerTests
+    {
+
+        [TestMethod]
+        public void Log_ValidMessage_SetLastMessage()
+        {
+            // Arrange
+            Logger logger = new Logger();
+
+            // Act
+            logger.Log("a");
+
+            // Assert
+            Assert.AreEqual("a", logger.LastMessage);
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Log_NullMessage_ThrowsArgumentNullException()
+        {
+            // Arrange
+            Logger logger = new Logger();
+
+            // Act
+            logger.Log(null);
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Log_EmptyMessage_ThrowsArgumentNullException()
+        {
+            // Arrange
+            Logger logger = new Logger();
+
+            // Act
+            logger.Log(string.Empty);
+
+        }
+
+        private string WhiteSpace => " ";
+
+        //private string Whitespace 
+        //{
+        //    get
+        //    {
+        //        return " ";
+        //    }
+        //}
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Log_WhiteSpaceMessage_ThrowsArgumentNullException()
+        {
+            // Arrange
+            Logger logger = new Logger();
+
+            // Act
+            logger.Log(WhiteSpace);
+        }
+
+    }
+}
