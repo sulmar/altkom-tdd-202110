a82f8fc510d877ebdecaed898bba342009706f99
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-10-05 12:43:52 +0200

    Dodanie przykładu z klientem TCP

---
+++ TestApp/Mocking/Target.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp.Mocking
+{
+    public abstract class Target
+    {
+
+    }
+
+    public class RCar : Target
+    {
+        public TargetStates State { get; private set; }
+
+        public void Push()
+        {
+            TcpClient client = new TcpClient();
+            client.Connect("127.0.0.1", 9000);
+
+            if (client.Connected)
+            {
+                if (State == TargetStates.Disabled)
+                {
+                    State = TargetStates.Enabled;
+                }
+                else
+                {
+                    State = TargetStates.Disabled;
+                }
+            }
+        }
+
+    }
+
+    public enum TargetStates
+    {
+        Enabled,
+        Disabled
+    }
+
+    
+}
