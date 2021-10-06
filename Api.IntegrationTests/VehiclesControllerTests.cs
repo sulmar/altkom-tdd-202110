1e4664870943aea68ecbdca74b623f9d5abd9092
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-10-06 13:04:35 +0200

    Utworzenie testu z TestServer

--- Api.IntegrationTests/VehiclesControllerTests.cs
+++ Api.IntegrationTests/VehiclesControllerTests.cs
@@ -1,9 +1,14 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Hosting;
 using System;
 using System.Net.Http;
 using Xunit;
 
 namespace Api.IntegrationTests
 {
+    // Install-Package Microsoft.AspNetCore.Mvc.Testing
+
     public class VehiclesControllerTests
     {
         // snippet: tm + 2 x Tab
@@ -12,16 +17,29 @@ namespace Api.IntegrationTests
         public void Get_ExistsId_ShouldReturnsOk()
         {
             // Arrange
-            HttpClient client = new HttpClient();
 
             int id = 1;
 
-            string url = $"https://localhost:44352/api/vehicles/{id}";
+            var hostBuilder = new HostBuilder()  // add Microsoft.Extensions.Hosting
+                .ConfigureWebHost(webHost =>
+                {
+                    webHost.UseTestServer(); // add using Microsoft.AspNetCore.TestHost;
+                    webHost.UseStartup<Api.Startup>(); // add using Microsoft.AspNetCore.Hosting;
+
+                });
+
+            var host = hostBuilder.Start();
+
+            // HttpClient client = new HttpClient();
+
+            HttpClient client = host.GetTestClient();
+
+            string url = $"/api/vehicles/{id}";
 
             // Act
             var result = client.GetAsync(url).Result;
 
-            // Assets
+            // Assert
             Assert.Equal(System.Net.HttpStatusCode.OK, result.StatusCode);
         }
 
@@ -30,16 +48,30 @@ namespace Api.IntegrationTests
         public void Get_NotExists_ShouldReturnsNotFound()
         {
             // Arrange
-            HttpClient client = new HttpClient();
-
             int id = int.MaxValue;
 
-            string url = $"https://localhost:44352/api/vehicles/{id}";
+            var hostBuilder = new HostBuilder()  // add Microsoft.Extensions.Hosting
+              .ConfigureWebHost(webHost =>
+              {
+                  webHost.UseTestServer(); // add using Microsoft.AspNetCore.TestHost;
+                    webHost.UseStartup<Api.Startup>(); // add using Microsoft.AspNetCore.Hosting;
+
+                });
+
+            var host = hostBuilder.Start();
+
+            // HttpClient client = new HttpClient();
+
+            HttpClient client = host.GetTestClient();
+
+           
+
+            string url = $"/api/vehicles/{id}";
 
             // Act
             var result = client.GetAsync(url).Result;
 
-            // Assets
+            // Assert
             Assert.Equal(System.Net.HttpStatusCode.NotFound, result.StatusCode);
         }
     }
