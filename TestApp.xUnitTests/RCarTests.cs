7906c6458abf28f7983eb501018c63ebb3119b29
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-10-06 11:25:58 +0200

    Utworzenie testów z użyciem Mocks

--- TestApp.xUnitTests/RCarTests.cs
+++ TestApp.xUnitTests/RCarTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Moq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,7 +14,7 @@ namespace TestApp.xUnitTests
         public bool Connected => true;
 
         public void Connect(string hostname, int port)
-        {            
+        {
         }
     }
 
@@ -23,6 +24,80 @@ namespace TestApp.xUnitTests
 
         public void Connect(string hostname, int port)
         {
+        }
+    }
+
+    public class RCarMockTests
+    {
+        [Fact]
+        public void Push_WhenConnect_ShouldSetsEnabledState()
+        {
+            // Arrange
+
+            Mock<ITcpClient> mockTcpClient = new Mock<ITcpClient>();
+
+            mockTcpClient
+                .Setup(tc => tc.Connect(It.IsAny<string>(), It.IsAny<int>()));
+
+            mockTcpClient
+                .SetupGet(tc => tc.Connected).Returns(true);
+
+            ITcpClient tcpClient = mockTcpClient.Object;
+
+            RCar target = new RCar(tcpClient);
+
+            // Act
+            target.Push();
+
+            // Assert
+            Assert.Equal(target.State, TargetStates.Enabled);
+        }
+
+        [Fact]
+        public void Push_WhenConnect_ShouldSetsDisableState()
+        {
+            // Arrange
+            Mock<ITcpClient> mockTcpClient = new Mock<ITcpClient>();
+
+            mockTcpClient
+             .Setup(tc => tc.Connect(It.IsAny<string>(), It.IsAny<int>()));
+
+            mockTcpClient
+                .SetupGet(tc => tc.Connected).Returns(true);
+
+            ITcpClient tcpClient = mockTcpClient.Object;
+
+            RCar target = new RCar(tcpClient);
+            target.Push();
+
+            // Act
+            target.Push();
+
+            // Assert
+            Assert.Equal(target.State, TargetStates.Disabled);
+        }
+
+        [Fact]
+        public void Push_WhenDisconnected_ShouldThrowApplicationException()
+        {
+            // Arrange
+            Mock<ITcpClient> mockTcpClient = new Mock<ITcpClient>();
+
+            mockTcpClient
+                .Setup(tc => tc.Connect(It.IsAny<string>(), It.IsAny<int>()));
+
+            mockTcpClient
+                .SetupGet(tc => tc.Connected).Returns(false);
+
+            ITcpClient tcpClient = mockTcpClient.Object;
+
+            RCar target = new RCar(tcpClient);
+
+            // Act
+            Action act = () => target.Push();
+
+            // Assert
+            Assert.Throws<ApplicationException>(act);
         }
     }
 
