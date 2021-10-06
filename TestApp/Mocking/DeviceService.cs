cbe248001ae0e964d5c2e77ccba3caa62fc4d656
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-10-06 08:35:33 +0200

    Dodanie przykładu

---
+++ TestApp/Mocking/DeviceService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp.Mocking
+{
+    public class DeviceService
+    {
+        public DeviceStatus GetStatus(int deviceId)
+        {
+            WebClient client = new WebClient();
+
+            string url = $"http://domain.com/devices/{deviceId}";
+
+            string xml = client.DownloadString(url);
+
+            if (xml == "<status>0</status>")
+            {
+                return DeviceStatus.Ready;
+            }
+            else if (xml == "<status>1</status>")
+            {
+                return DeviceStatus.Cooling;
+            }
+            else if (xml == "<status>2</status>")
+            {
+                return DeviceStatus.Heating;
+            }
+            else
+            {
+                throw new NotSupportedException();
+            }
+        }
+    }
+
+    public enum DeviceStatus
+    {
+        Ready,
+        Cooling,
+        Heating
+    }
+}
