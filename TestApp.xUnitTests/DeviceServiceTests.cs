ebe3840ee90525914b2f46f9ea7f407bc0d6c397
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-10-06 11:12:44 +0200

    Dodanie testów z mock

---
+++ TestApp.xUnitTests/DeviceServiceTests.cs
@@ -0,0 +1,69 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestApp.Mocking;
+using Xunit;
+
+namespace TestApp.xUnitTests
+{
+
+    public class DeviceServiceTests
+    {
+        private Mock<IWebClient> mockWebClient;
+        private DeviceService deviceService;
+
+        public DeviceServiceTests()
+        {
+            this.mockWebClient = new Mock<IWebClient>();
+            this.deviceService = new DeviceService( mockWebClient.Object);
+        }
+
+        [Fact]
+        public void GetStatus_Ready_ShouldReturnsReadyStatus()
+        {
+            // Arrange
+            mockWebClient
+                .Setup(wc => wc.DownloadString(It.IsAny<string>()))
+                .Returns("<status>0</status>");
+
+            // Act
+            var result  = deviceService.GetStatus(1);
+
+            // Assets
+            Assert.Equal(DeviceStatus.Ready, result);
+        }
+
+        [Fact]
+        public void GetStatus_Cooling_ShouldReturnsReadyStatus()
+        {
+            // Arrange
+            mockWebClient
+                .Setup(wc => wc.DownloadString(It.IsAny<string>()))
+                .Returns("<status>1</status>");
+
+            // Act
+            var result = deviceService.GetStatus(1);
+
+            // Assets
+            Assert.Equal(DeviceStatus.Cooling, result);
+        }
+
+        [Fact]
+        public void GetStatus_Heating_ShouldReturnsReadyStatus()
+        {
+            // Arrange
+            mockWebClient
+                .Setup(wc => wc.DownloadString(It.IsAny<string>()))
+                .Returns("<status>2</status>");
+
+            // Act
+            var result = deviceService.GetStatus(1);
+
+            // Assets
+            Assert.Equal(DeviceStatus.Heating, result);
+        }
+    }
+}
--- TestApp/Mocking/DeviceService.cs
+++ TestApp/Mocking/DeviceService.cs
@@ -3,36 +3,61 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace TestApp.Mocking
 {
+    public interface IWebClient
+    {
+        string DownloadString(string url);
+    }
+
+    public class StandardWebClient : IWebClient
+    {
+        private WebClient client = new WebClient();
+
+        public string DownloadString(string url) => client.DownloadString(url);
+    }
+
     public class DeviceService
     {
+        private readonly IWebClient client;
+
+        public DeviceService()
+            : this(new StandardWebClient())
+        {
+
+        }
+
+        public DeviceService(IWebClient client)
+        {
+            this.client = client;
+        }
+
         public DeviceStatus GetStatus(int deviceId)
         {
-            WebClient client = new WebClient();
-
             string url = $"http://domain.com/devices/{deviceId}";
 
             string xml = client.DownloadString(url);
 
-            if (xml == "<status>0</status>")
-            {
-                return DeviceStatus.Ready;
-            }
-            else if (xml == "<status>1</status>")
-            {
-                return DeviceStatus.Cooling;
-            }
-            else if (xml == "<status>2</status>")
-            {
-                return DeviceStatus.Heating;
-            }
-            else
-            {
-                throw new NotSupportedException();
-            }
+            return Map(xml);
+        }
+
+        private static DeviceStatus Map(string xml)
+        {
+            Regex regex = new Regex(@"^<status>(\d)<\/status>");
+
+            Match match = regex.Match(xml);
+
+            string statusString = match.Groups[1].Value;
+
+            int status = int.Parse(statusString);
+
+            DeviceStatus deviceStatus = (DeviceStatus)status;
+
+            return deviceStatus;
+            
         }
     }
 
